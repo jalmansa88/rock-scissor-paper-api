449303c1fd8b96d91395636b0373c59d44555fee
jalmansa <jalmansa88@gmail.com>
2020-08-10 19:46:30 +0200

    init project

---
+++ src/main/java/com/almansa/cyklumcodechallengeapi/CyklumCodechallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.almansa.cyklumcodechallengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyklumCodechallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyklumCodechallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almansa/cyklumcodechallengeapi/CyklumCodechallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almansa.cyklumcodechallengeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyklumCodechallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
