246b8567d50f89862cdf57c4182fb24128a06fcb
jalmansa <jalmansa88@gmail.com>
2020-08-13 21:22:34 +0200

    enabling cors

--- src/main/java/com/almansa/ciklumcodechallengeapi/CiklumCodechallengeApiApplication.java
+++ src/main/java/com/almansa/ciklumcodechallengeapi/CiklumCodechallengeApiApplication.java
@@ -1,13 +1,32 @@
 package com.almansa.ciklumcodechallengeapi;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CiklumCodechallengeApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiklumCodechallengeApiApplication.class, args);
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "DELETE"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
